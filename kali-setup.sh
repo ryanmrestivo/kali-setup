a66e05d1c4520ba30bcd984113012ffe0a5c32cf
Ryan Restivo <53523282+ryanmrestivo@users.noreply.github.com>
2022-04-09 11:49:17 -0400

    script cleanup

--- kali-setup.sh
+++ kali-setup.sh
@@ -8,42 +8,19 @@
 #Prevent linux from going to sleep
 #power manager -> display -> slide everything left to "never"
 
-# Metasploit notes...  Armitage is broken on Kali 2021.x
-#wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run && wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run.sha1 && echo $(cat metasploit-latest-linux-x64-installer.run.sha1)'  'metasploit-latest-linux-x64-installer.run > metasploit-latest-linux-x64-installer.run.sha1 && shasum -c metasploit-latest-linux-x64-installer.run.sha1 && chmod +x ./metasploit-latest-linux-x64-installer.run && sudo ./metasploit-latest-linux-x64-installer.run
-# port 3790?
-# if wget failed makes sure you are using wget-ssl
-# if install hangs at 100% run: "sudo bash /opt/metasploit/ctlscript.sh restart"
-# MSF_DATABASE_CONFIG=/opt/metasploit-framework/database.yml
-# "database.yml" should go directly into "metasploit-framework". Also in terminal type the command line: MSF_DATABASE_CONFIG=/opt/metasploit-framework/database.yml
-# Also make sure you go into a file called "profile" its in root/etc/profile. Profile is a text document, at the bottom add the line:
-# export MSF_DATABASE_CONFIG=/opt/metasploit-framework/database.yml
-
 echo "\n\n\n System Update & Setup"
 
 echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
-echo ..
-echo ...
-echo ....
-echo ...
-echo ..
-echo "\n\n\n Quiet update + minimal interaction to prevent bugs."
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
+
 sudo -E apt -y update
 yes | sudo apt upgrade
-python3 -m pip install --upgrade pip
-sudo -E apt-get -y autoclean
+
 ln -s /opt/ /home/kali/Desktop/opt
 
-# Add search cache
-sudo apt-cache search kali
-
-# Install Git - Should already be installed
-echo "\n\n\n Installing - Git \n"
 sudo apt-get install -y git 
 
-# Install Perl
-echo "\n\n\n Installing - perl \n"
 sudo apt-get install -y perl  
 
 # Install Go - https://golang.org/doc/install
@@ -71,6 +48,7 @@ python2 -m pip install pipenv
 python3 -m pip install pipenv
 python3 -m pip install mitmproxy
 sudo apt-get install -y gdu
+sudo apt install nodejs
 
 # Install searchsploit
 echo "\n\n\n Installing - searchsploit \n"
@@ -92,25 +70,6 @@ cd /opt/
 sudo git clone https://github.com/21y4d/nmapAutomator.git /opt/nmapautomator/
 # Creates a Symbolic Link to the file so you can call it from anywhere
 sudo ln -s /opt/nmapautomator/nmapAutomator.sh /usr/local/bin/
-
-# Install Reconbot - https://github.com/0bs3ssi0n/Reconbot
-echo "\n\n\n Installing - Reconbot \n"
-cd /opt/
-sudo git clone https://github.com/0bs3ssi0n/Reconbot /opt/reconbot/
-# Creates a Symbolic Link to the file so you can call it from anywhere
-sudo ln -s /opt/reconbot/reconbot.sh /usr/local/bin/
-
-echo "\n\n\n Downloading - J3ssie/metabigor \n"
-# https://github.com/j3ssie/metabigor
-git clone  https://github.com/j3ssie/metabigor.git /opt/metabigor
-
-echo "\n\n\n Downloading - streetsec/Sudomy \n"
-# https://github.com/screetsec/Sudomy
-git clone https://github.com/screetsec/Sudomy.git /opt/sudomy
-
-echo "\n\n\n Downloading - nahamsec/lazyrecon \n"
-# https://github.com/nahamsec/lazyrecon
-git clone https://github.com/nahamsec/lazyrecon.git /opt/lazyrecon
 
 echo "\n\n\n Downloading - trustedsec/unicorn \n"
 # https://github.com/trustedsec/unicorn
@@ -136,23 +95,10 @@ echo "\n\n\n Downloading - aboul3la/Sublist3r \n"
 # https://github.com/aboul3la/Sublist3r
 git clone https://github.com/aboul3la/Sublist3r.git /opt/sublister
 
-
 sudo git clone https://github.com/g0tmi1k/msfpc /opt/msfvenom-payload-creator
 # https://github.com/g0tmi1k/msfpc
 curl -k -L "https://raw.githubusercontent.com/g0tmi1k/mpc/master/msfpc.sh" > /usr/local/bin/msfpc
 chmod 0755 /usr/local/bin/msfpc
-
-echo "\n\n\n Downloading - Gr1mmie/sumrecon \n"
-# https://github.com/Gr1mmie/sumrecon
-sudo git clone https://github.com/Gr1mmie/sumrecon.git /opt/sumrecon
-
-echo "\n\n\n Downloading - danielmiessler/SecLists \n"
-# https://github.com/danielmiessler/SecLists
-sudo git clone https://github.com/danielmiessler/SecLists /opt/seclists
-
-echo "\n\n\n Downloading - pwncat \n"
-# https://github.com/calebstewart/pwncat
-sudo pip install git+https://github.com/calebstewart/pwncat.git
 
 echo "\n\n\n Downloading - r00t-3xp10it/resource_files (mosquito) \n"
 # https://github.com/r00t-3xp10it/resource_files
@@ -165,8 +111,10 @@ echo "\n\n\n Downloading - One-Lin3r \n"
 sudo git clone https://github.com/D4Vinci/One-Lin3r.git /opt/one-liner
 pip install one-lin3r
 
-echo "\n\n\n Downloading - tomnomnom/assetfinder \n"
-go get -u github.com/tomnomnom/assetfinder
+echo "\n\n\n Downloading - knassar702/scant3r \n"
+ git clone https://github.com/knassar702/scant3r /opt/scanter
+ cd scant3r
+ pip3 install -r requirements.txt
 
 echo "\n\n\n Installing - m4ll0k/takeover \n"
 git clone https://github.com/m4ll0k/takeover.git /opt/takeover
@@ -177,6 +125,14 @@ echo "\n\n\n Installing - ristocratos/bashtop \n"
 git clone https://github.com/aristocratos/bashtop.git /opt/bashtop
 cd /opt/bashtop/DEB
 ./build
+
+echo "\n\n\n Installing - 0xApt/awesome-bbht \n"
+git clone https://github.com/0xApt/awesome-bbht.sh /opt/bbht
+cd bbht
+chmod +x awesome-bbht.sh
+
+cd awesome-bbht
+chmod +x awesome-bbht.sh
 
 echo "\n\n\n Installing - darkoperator/dnsrecon \n"
 git clone https://github.com/darkoperator/dnsrecon.git /opt/dnsrecon
@@ -212,12 +168,6 @@ cd /opt/eternalscanner
 apt install masscan wget python-pip python-crypto python-impacket python-pyasn1-modules netcat
 pip install crypto && pip install impacket && pip install pyasn1-modules
 chmod +x escan
-	
-echo "\n\n\n Installing - robotshell/magicRecon.git \n"
-git clone https://github.com/robotshell/magicRecon.git /opt/magicrecon
-cd /opt/magicrecon
-chmod +x install.sh
-./install.sh
 
 echo "\n\n\n Installing - Cyb0r9/ispy.git \n"
 git clone https://github.com/Cyb0r9/ispy.git /opt/ispy
@@ -230,11 +180,6 @@ git clone https://github.com/skavngr/rapidscan.git /opt/rapidscan
 cd /opt/rapidscan
 chmod +x rapidscan.py
 
-echo "\n\n\n Installing - leebaird/discover.git \n"
-git clone https://github.com/leebaird/discover.git /opt/discover
-cd /opt/discover/
-./update.sh
-
 echo "\n\n\n Installing - trimstray/sandmap \n"
 git clone https://github.com/trimstray/sandmap.git /opt/sandmap
 cd /opt/sandmap
@@ -246,25 +191,6 @@ echo "\n\n\n Installing - smicallef/spiderfoot \n"
 git clone https://github.com/smicallef/spiderfoot.git /opt/spiderfoot
 cd /opt/spiderfoot
 pip3 install -r requirements.txt
-
-echo "\n\n\n Installing - smicallef/spiderfoot \n"
-git clone https://github.com/smicallef/spiderfoot.git /opt/mosquito
-cd /opt/mosquito
-git clone https://github.com/r00t-3xp10it/resource_files.git
-./mosquito.sh -i
-
-echo "\n\n\n Installing - Gr1mmie/sumrecon \n"
-git clone https://github.com/VainlyStrain/Vailyn.git /opt/Vailyn
-cd /opt/Vailyn
-pip install -r requirements.txt 
-
-echo "\n\n\n Installing - 21y4d/nmapAutomator \n"
-git clone https://github.com/21y4d/nmapAutomator.git /opt/nmapAutomator
-
-echo "\n\n\n Installing - saeeddhqan/Maryam \n"
-git clone https://github.com/saeeddhqan/Maryam.git /opt/maryam
-cd /opt/maryam
-pip install maryam
 
 echo "\n\n\n Installing - KeepWannabe/Remot3d \n"
 git clone https://github.com/KeepWannabe/Remot3d.git /opt/remot3d
@@ -284,16 +210,14 @@ git clone https://github.com/scipag/vulscan.git /opt/vulscan
 ln -s `pwd`/vulscan /usr/share/nmap/scripts/vulscan
 # may need to change directory, will test.  See shortcuts for commands.
 
-echo "\n\n\n Installing - 1N3/Sn1per \n"
-git clone https://github.com/1N3/Sn1per.git /opt/sniper
-cd /opt/Sn1per
-chmod +x install.sh
-./install.sh
-
 echo "\n\n\n Installing - j3ssie/Osmedeus \n"
 git clone https://github.com/j3ssie/Osmedeus.git /opt/osmedeus
 cd /opt/osmedeus/
-./install.sh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/osmedeus/osmedeus-base/master/install.sh)"
+mkdir -p $GOPATH/src/github.com/j3ssie
+git clone --depth=1 https://github.com/j3ssie/osmedeus $GOPATH/src/github.com/j3ssie/osmedeus
+cd $GOPATH/src/github.com/j3ssie/osmedeus
+make build
 
 echo "\n\n\n Installing - 0xinfection/tidos-framework \n"
 # cd /opt/Osmedeus && python3 Osmedeus.py
@@ -301,20 +225,6 @@ git clone https://github.com/0xinfection/tidos-framework.git /opt/tidos-framewor
 cd /opt/tidos-framework
 apt -y install libncurses5 libxml2 nmap tcpdump libexiv2-dev buildssential python3-pip libmariadbclient18 libmysqlclient-dev tor konsole
 pip3 install -r requirements.txt
-
-echo "\n\n\n Installing - Yukinoshita47/Yuki-Chan-The-Auto-Pentest \n"
-git clone https://github.com/Yukinoshita47/Yuki-Chan-The-Auto-Pentest.git /opt/yuki-chan
-cd /opt/yuki-chan
-chmod 777 wafninja joomscan install-perl-module.sh yuki.sh
-chmod 777 Module/WhatWeb/whatweb
-pip install -r requirements.txt
-./install-perl-module.sh
-
-echo "\n\n\n Installing - securethelogs/Exnoscan \n"  
-git clone https://github.com/securethelogs/Exnoscan.git /opt/exnoscan
-cd /opt/exnoscan
-chmod +x exnoscan.sh
-./exnoscan.sh
 
 echo "\n\n\n Installing - alexandreborges/malwoverview \n"
 git clone https://github.com/alexandreborges/malwoverview.git /opt/malwoverview
@@ -345,42 +255,41 @@ cd /opt/cr3dOv3r
 python3 -m pip install -r requirements.txt
 # python3 Cr3d0v3r.py -h
 
+git clone https://github.com/chvancooten/BugBountyScanner.git /opt/BugBountyScanner
+cd BugBountyScanner
+# cp .env.example .env # Edit accordingly
+chmod +x BugBountyScanner.sh setup.sh
+./setup.sh -t /custom/tools/dir # Setup is automatically triggered, but can be manually run
+# ./BugBountyScanner.sh --help
+# ./BugBountyScanner.sh -d target1.com -d target2.net -t /custom/tools/dir --quick
+
+echo "\n\n\n Installing - leebaird/discover \n"
+git clone https://github.com/leebaird/discover /opt/discover/
+cd /opt/discover/
+./update.sh
+
 echo "\n\n\n Installing - BC-SECURITY/Empire \n"
 git clone https://github.com/BC-SECURITY/Empire.git /opt/empire
 cd /opt/empire/setup/
 ./install.sh    
 
-echo "\n\n\n Installing - Gr1mmie/sumrecon \n"
-apt -y install libssl-dev libffi-dev python-dev buildssential
-git clone https://github.com/byt3bl33d3r/CrackMapExec.git /opt/crackmapexec
-cd /opt/crackmapexec
-apt install python3.9-venv
+echo "\n\n\n Installing - screetsec/Sudomy \n"
+git clone --recursive https://github.com/screetsec/Sudomy.git /opt/Sudomy
+cd Sudomy
+python3 -m pip install -r requirements.txt
 
-echo "\n\n\n Installing - chvancooten/BugBountyScanner \n"
-git clone https://github.com/chvancooten/BugBountyScanner.git /opt/bugbountyscanner
-cd /opt/bugbountyscanner
-chmod +x BugBountyScanner.sh setup.sh
-./setup.sh
-# ./BugBountyScanner.sh --help
-
-echo "\n\n\n Installing - impacket \n"
-sudo git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket/
-cd /opt/impacket/
-sudo pip install /opt/impacket/.
+echo "\n\n\n Installing - VainlyStrain/Vailyn \n"
+git clone https://github.com/VainlyStrain/Vailyn
+cd /opt/Vailyn
+pip install -r requirements.txt
 
 echo "\n\n\n Installing - eslam3kl/3klCon \n"
 sudo git clone https://github.com/eslam3kl/3klCon.git /opt/3lckon/
 cd /opt/3lckon/
 chmod +x install_tools.sh
 ./install_tools.sh
-
-echo "\n\n\n Unzipping RockYou \n"
-gunzip /usr/share/wordlists/rockyou.txt.gz 2>/dev/null
-ln -s /usr/share/wordlists ~/Downloads/wordlists 2>/dev/null
-
-echo "\n\n\n Setup - scipag/vulscan.git \n"
-git clone https://github.com/scipag/vulscan.git /usr/share/nmap/scripts/vulscan
-ln -s `pwd`/scipag_vulscan /usr/share/nmap/scripts/vulscan
+apt-get install jq nmap phantomjs npm chromium parallel
+npm i -g wappalyzer wscat
 
 echo "\n\n\n Installing - r00t-3xp10it/venom \n"
 # https://github.com/r00t-3xp10it/venom
@@ -390,10 +299,6 @@ cd opt/venom
 sudo find ./ -name "*.sh" -exec chmod +x {} \;
 sudo find ./ -name "*.py" -exec chmod +x {} \;
 cd aux && sudo ./setup.sh
-
-echo "\n\n\n Downloading - zerosum0x0/koadic \n"
-# https://github.com/zerosum0x0/koadic
-git clone https://github.com/zerosum0x0/koadic.git /opt/koadic
 
 # vulsvan
 # nmap -sV --script=vulscan/vulscan.nse scanme.nmap.org
@@ -406,10 +311,7 @@ git clone https://github.com/zerosum0x0/koadic.git /opt/koadic
 # /opt/CHAOS/client/main.go
 # After running go to http://localhost:8080 and login with the default username: ***admin*** and password: ***changeme***.
 
-
-
 # Setup Docker Tools
-
 
 # docker container ls -a
 # docker container management
@@ -420,32 +322,30 @@ git clone https://github.com/zerosum0x0/koadic.git /opt/koadic
 #    portainer/portainer-ce:latest
 # 0.0.0.0:9443 should be the default URL.
 
-docker pull iknowjason/aria-base:latest
+# docker pull iknowjason/aria-base:latest
 # docker run -ti iknowjason/aria-base:latest
-docker pull shmilylty/oneforall
+# docker pull shmilylty/oneforall
 # docker run -it --rm shmilylty/oneforall
-docker pull offensivedockerfiles/v3n0m-scanner
+# docker pull offensivedockerfiles/v3n0m-scanner
 # docker run -it offensivedockerfiles/v3n0m-scanner
-docker pull offensivedockerfiles/gitminer
+# docker pull offensivedockerfiles/gitminer
 # docker run -it offensivedockerfiles/gitminer
-docker pull koutto/jok3r
+# docker pull koutto/jok3r
 # docker run -i -t --name jok3r-container -w /root/jok3r -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --shm-size 2g --net=host koutto/jok3r
-docker pull shmilylty/oneforall
+# docker pull shmilylty/oneforall
 # docker run -it --rm shmilylty/oneforall
-docker pull vainlystrain/tidos-framework
+# docker pull vainlystrain/tidos-framework
 # docker run -it vainlystrain/tidos-framework
-docker pull rustscan/rustscan
+# docker pull rustscan/rustscan
 # docker run -it rustscan/rustscan
 
 # docker image ls
-
-
 
 # Setup File Strucutres 
 sudo chmod -R 755 /opt 
 
 # Cleanup
-echo "\n\n\n Updating - RUnning FInal UPDATE CHECK! \n"
+echo "\n\n\n Updating - RUNNING FINAL UPDATE CHECK! \n"
 sudo apt update
 sudo apt upgrade -y
 sudo apt upgrade -y
--- update.sh
+++ update.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env bash
-tput setaf 5;echo "##############################"
-tput setaf 5;echo "#        Updates only        #"
-tput setaf 5;echo "##############################"
 
-echo "[+] Update + Upgrade System.."
+
 sudo apt update -qq 
 sudo apt upgrade -qq -y
 sudo npm install npm@latest -g
 sudo npm update -g
-
-echo "[+] Upgrading Kali version to latest..."
 sudo apt dist-upgrade -qq -y
 sudo apt full-upgrade -qq -y
 
-echo Complete.
-
-# echo "[+] 202x.x OVA fixup..."
-
-# Fix: ORIG_HEAD broken reference
-# sudo find /usr/share/ -name ORIG_HEAD -size -1b -delete
